a9ea7275359807889922833593a5f4c9e2dac423
Mahmoud Hamam <mahmoudhamam093@gmail.com>
2026-02-15 22:36:23 +0200

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,63 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(left: 20, top: 24, bottom: 24),
-      decoration: BoxDecoration(
-        color: Color(0xffFFCC55),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            // Note: ListTile is a widget that displays a single row with some text and an optional leading and trailing icon.
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.w500,
-                color: Colors.black,
-              ),
-            ),
-
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text(
-                'Build your career with Tharwat samy',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(left: 20, top: 24, bottom: 24),
+        decoration: BoxDecoration(
+          color: Color(0xffFFCC55),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              // Note: ListTile is a widget that displays a single row with some text and an optional leading and trailing icon.
+              title: Text(
+                'Flutter Tips',
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 26,
                   fontWeight: FontWeight.w500,
-                  color: Colors.black.withOpacity(0.6),
+                  color: Colors.black,
+                ),
+              ),
+
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  'Build your career with Tharwat samy',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black.withOpacity(0.6),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
+            Padding(
+              padding: const EdgeInsets.only(right: 33, top: 16),
+              child: Text(
+                'may 20, 2024',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.6),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 33, top: 16),
-            child: Text(
-              'may 20, 2024',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.6),
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
