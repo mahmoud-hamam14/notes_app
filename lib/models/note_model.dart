c92b6a5c6c7f64e8def753a3660b4a1c7a8500fb
Mahmoud Hamam <mahmoudhamam093@gmail.com>
2026-02-17 02:41:15 +0200

    Generate type Adapter

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,22 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart'; // This line is necessary for Hive to generate the adapter code for this model. 
+
+@HiveType(
+  typeId: 0,
+) // This annotation tells Hive that this class is a Hive object and assigns it a unique type ID.
+class NoteModel extends HiveObject {
+  @HiveField(
+    0,
+  ) // This annotation tells Hive that this field should be stored in the database and assigns it a unique field ID.
   final String title;
+
+  @HiveField(1)
   final String subtitle;
+
+  @HiveField(2)
   final String date;
+
+  @HiveField(3)
   final int color;
 
   NoteModel({
