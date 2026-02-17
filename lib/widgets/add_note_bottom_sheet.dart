59c15151ce5cc5937e9b05b010f21aed9497d572
Mahmoud Hamam <mahmoudhamam093@gmail.com>
2026-02-17 04:16:38 +0200

    Take input and validate data

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -7,26 +7,74 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 32),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(child: AddNoteForm()),
+    );
+  }
+}
 
-            CustomTextField(hintText: 'title'),
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({super.key});
 
-            SizedBox(height: 16),
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
 
-            CustomTextField(hintText: 'content', maxLines: 5),
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  // Create a GlobalKey to identify the form and access its state. This is necessary for form validation and other form-related operations.
 
-            SizedBox(height: 32),
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  // Initialize the autovalidateMode variable to AutovalidateMode.disabled. This is used to control when the form should automatically validate its fields.
 
-            CustomBottom(),
+  String? title, subtitle;
 
-            SizedBox(height: 16),
-          ],
-        ),
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(height: 32),
+
+          CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
+
+            hintText: 'title',
+          ),
+
+          const SizedBox(height: 16),
+
+          CustomTextField(
+            onSaved: (value) {
+              subtitle = value;
+            },
+
+            hintText: 'subtitle',
+            maxLines: 5,
+          ),
+
+          const SizedBox(height: 32),
+
+          CustomBottom(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+                // If the form is valid, save the form state. This will trigger the onSaved callbacks for each form field, allowing you to capture the input values.
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+                // If the form is not valid, set the autovalidateMode to AutovalidateMode.always and call setState to trigger a rebuild of the widget. This will cause the form to automatically validate its fields and display any validation errors.
+              }
+            },
+          ),
+
+          const SizedBox(height: 16),
+        ],
       ),
     );
   }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -2,15 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hintText, this.maxLines = 1});
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    this.maxLines = 1,
+    this.onSaved,
+  });
 
   final String hintText;
 
   final int maxLines;
 
+  final void Function(String?)?
+  onSaved; // Define an optional onSaved callback that takes a String? parameter. This allows the parent widget to handle the saving of the text field's value when the form is saved.
+
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved:
+          onSaved, // Set the onSaved callback to the onSaved parameter passed to the CustomTextField. This allows the parent widget to handle the saving of the text field's value when the form is saved.
+
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'This field is required';
+        } else {
+          return null;
+        }
+      }, // Add a validator function to the TextFormField. This function checks if the value is null or empty, and if so, returns an error message. Otherwise, it returns null, indicating that the input is valid.
+
       maxLines: maxLines,
       cursorColor: kPrimaryColor,
       decoration: InputDecoration(
