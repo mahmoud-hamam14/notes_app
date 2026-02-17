704fc221e179e488eff12b878702be197084369a
Mahmoud Hamam <mahmoudhamam093@gmail.com>
2026-02-17 02:59:54 +0200

    init hive and open notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter(); // Initialize Hive for Flutter. This is necessary before using Hive to store data in the app.
+
+  await Hive.openBox(
+    kNotesBox,
+  ); // Open the Hive box named 'notes_box'. This is necessary to access the data stored in this box.
+
   runApp(const NotesApp());
 }
 
